6242f01ffebe44dce394ae575bfce7ec43893b99
lanlq2009 <lanlq2009@gmail.com>
2021-04-01 06:15:26 -0300

    Commit inicial do projeto;

---
+++ src/main/java/br/com/pontointeligente/api/PontoInteligenteIiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pontointeligente/api/PontoInteligenteIiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
