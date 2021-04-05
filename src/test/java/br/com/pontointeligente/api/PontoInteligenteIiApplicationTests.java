f61e5c454d7b1bdfd03f21a3f29854e0574ecf7e
lanlq2009 <lanlq2009@gmail.com>
2021-04-04 22:43:35 -0300

    Configurando o Junit 5 para atender o spring boot 2.4.4;

--- src/test/java/br/com/pontointeligente/api/PontoInteligenteIiApplicationTests.java
+++ src/test/java/br/com/pontointeligente/api/PontoInteligenteIiApplicationTests.java
@@ -1,12 +1,10 @@
 package br.com.pontointeligente.api;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 @ActiveProfiles("test")
 class PontoInteligenteIiApplicationTests {
--- src/test/java/br/com/pontointeligente/api/repository/EmpresaRepositoryTest.java
+++ src/test/java/br/com/pontointeligente/api/repository/EmpresaRepositoryTest.java
@@ -3,20 +3,17 @@ package br.com.pontointeligente.api.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.pontointeligente.api.builder.EmpresaBuilder;
 import br.com.pontointeligente.api.entities.Empresa;
 import br.com.pontointeligente.api.repositories.EmpresaRepository;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test") 
 public class EmpresaRepositoryTest {
@@ -27,14 +24,14 @@ public class EmpresaRepositoryTest {
 	
 	private static final String CNPJ = "52999306000105";
 	
-	@Before
+	@Before(value="")
 	public void setUp() {
 		Empresa empresa = new EmpresaBuilder().comCnpj(CNPJ).comRazaoSocial("Empresa Teste S.A.").build();
 		this.empresaRepository.save(empresa);
 	}
 	
 	
-	@After
+	@After(value="")
 	public void setOff() {
 		this.empresaRepository.deleteAll();
 	}
